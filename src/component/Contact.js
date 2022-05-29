9ceeed58532c94727190327c041ca95dfc7a67d0
Ugboaja Uchechi <74814780+Ugboaja-Uchechi@users.noreply.github.com>
2022-05-29 18:23:40 +0100

    Style form area

--- src/component/Contact.js
+++ src/component/Contact.js
@@ -3,17 +3,27 @@ import React from "react";
 const Contact = () => {
   return (
     <>
-      <footer>
+      <footer className="height">
         <h2 className="headings">Get in <span>touch.</span></h2>
         <p id="footer-p">Wanna work together?, have any question or proposal?, or just want to say Hello. Go ahead. I would love to hear from you!</p>
         <form>
-          <label for="name" className="grid1 grid-row1">Name</label>
-          <input type="text" id="name" name="name" required className="grid1 grid-row2"></input>
-          <label for="name" className="grid2 grid-row1">Email</label>
-          <input type="email" id="email" name="email" required className="grid2 grid-row2"></input>
-          <label for="message">Message</label>
-          <textarea id="message" for="message" rows="4" cols="50" required></textarea>
-          <button type="submit">Send a Message</button>
+          <div id="input-flex">
+            <div>
+              <label for="name">Name</label> <br />
+              <input type="text" id="name" name="name" required />
+            </div>
+            <div>
+              <label for="email">Email</label> <br />
+              <input type="email" id="email" name="email" required />
+            </div>
+          </div>
+          <div className="textarea-flex">
+            <label for="message">Message</label>
+            <textarea id="message" for="message" rows="4" cols="50" required></textarea>
+          </div>
+          <div className="textarea-flex">
+            <button type="submit" className="grid4">Send a Message</button>
+          </div>
         </form>
       </footer>
     </>
