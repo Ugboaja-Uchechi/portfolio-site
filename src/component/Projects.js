05955d06dfdde741e6960dbd888044818cab9671
Ugboaja Uchechi <74814780+Ugboaja-Uchechi@users.noreply.github.com>
2022-06-11 14:58:11 +0100

    Write short note on each project

--- src/component/Projects.js
+++ src/component/Projects.js
@@ -29,7 +29,7 @@ const Projects = () => {
             </div>
             <div>
               <h3>Vacay Home</h3>
-              <p></p>
+              <p>Vacay home is a rental site built with React, Ruby on Rails, Bootstrap, Jest, and CSS in which users can create reservations for vacation homes and create listings.</p>
             </div>
           </div>
           <div id="grid-column-3">
@@ -37,8 +37,8 @@ const Projects = () => {
               <img src={SpaceImage} alt="A girl smiling" className="width margin-start"></img>
             </div>
             <div>
-              <h3>Space-X Hub</h3>
-              <p></p>
+              <h3>Space Traveler's Hub</h3>
+              <p>Space Traveler's Hub is a desktop web application built with React, Bootstrap, Jest, and, CSS that allow users to book rockets and join selected space missions.</p>
             </div>
           </div>
           <div id="grid-row-3">
@@ -47,7 +47,7 @@ const Projects = () => {
             </div>
             <div>
               <h3>IP Address Tracker</h3>
-              <p></p>
+              <p>IP Address Tracker is a web applcation built with JavaScript, Api, HTML, and, CSS. Users can see their IP Address on the map on the initial page load. Search for any IP addresses or domains and see the key information and location. View the optimal layout for each page depending on their device screen size.</p>
             </div>
           </div>
           <div id="grid-row-4">
@@ -56,7 +56,7 @@ const Projects = () => {
             </div>
             <div>
               <h3>URL Shortening API</h3>
-              <p></p>
+              <p>URL Shortening API is a web app built with JavaScript, Api, HTML, and, CSS. Users can shorten any valid URL, see a list of their shortened links, even after refreshing the browser, copy the shortened link to their clipboard in a single click</p>
             </div>
           </div>
         </div>
