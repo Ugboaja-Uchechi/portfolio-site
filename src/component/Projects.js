f1b8470e95f5255901cbb305d16fdcd10ea00201
Ugboaja Uchechi <74814780+Ugboaja-Uchechi@users.noreply.github.com>
2022-06-14 15:24:07 +0100

    Re-style projects section

--- src/component/Projects.js
+++ src/component/Projects.js
@@ -1,8 +1,8 @@
 import React, { useEffect } from "react";
-import VacayImage from "../images/vacay.png";
-import SpaceImage from "../images/space-x.png";
-import IpImage from "../images/ip-tracker.png";
-import UrlImage from "../images/url-shortening.png";
+// import VacayImage from "../images/vacay.png";
+// import SpaceImage from "../images/space-x.png";
+// import IpImage from "../images/ip-tracker.png";
+// import UrlImage from "../images/url-shortening.png";
 import gsap from "gsap";
 import {ScrollTrigger} from 'gsap/ScrollTrigger'
 gsap.registerPlugin(ScrollTrigger)
@@ -17,46 +17,62 @@ const Projects = () => {
 
   return (
     <>
-      <section>
-      <div id="h2-cover">
-        <h2 className="headings">Selected</h2>
-        <h2 className="headings" id="span">Projects</h2>
-      </div>
-        <div className="grid-container">
-          <div id="grid-column-2">
-            <div>
-              <img src={VacayImage} alt="A girl smiling" className="width margin-start"></img>
-            </div>
-            <div>
-              <h3>Vacay Home</h3>
-              <p>Vacay home is a rental site built with React, Ruby on Rails, Bootstrap, Jest, and CSS in which users can create reservations for vacation homes and create listings.</p>
-            </div>
-          </div>
-          <div id="grid-column-3">
-            <div>
-              <img src={SpaceImage} alt="A girl smiling" className="width margin-start"></img>
-            </div>
-            <div>
-              <h3>Space Traveler's Hub</h3>
-              <p>Space Traveler's Hub is a desktop web application built with React, Bootstrap, Jest, and, CSS that allow users to book rockets and join selected space missions.</p>
+      <section className="proj-section">
+        <div id="h2-cover">
+          <h2 className="headings">Selected</h2>
+          <h2 className="headings" id="span">Projects</h2>
+        </div>
+        <div className="flex-container grid-container">
+          <div id="grid-column-2" className="flex-cover">
+            <div className="seyi-div div-1">
+              <div className="content">
+                <h3 className="project-title">Vacay Home</h3>
+                <p>Vacay home is a rental site built with React, Ruby on Rails, Bootstrap, Jest, and CSS in which users can create reservations for vacation homes and create listings.</p>
+              </div>
+              <div className="img">
+                {/* <img src={VacayImage} alt="A girl smiling" className="width" /> */}
+              </div>
+              {/* <div className="content">
+                <h3 className="project-title">Vacay Home</h3>
+                <p>Vacay home is a rental site built with React, Ruby on Rails, Bootstrap, Jest, and CSS in which users can create reservations for vacation homes and create listings.</p>
+              </div> */}
             </div>
           </div>
-          <div id="grid-row-3">
-            <div>
-              <img src={IpImage} alt="A girl smiling" className="width margin-start"></img>
-            </div>
-            <div>
-              <h3>IP Address Tracker</h3>
-              <p>IP Address Tracker is a web applcation built with JavaScript, Api, HTML, and, CSS. Users can see their IP Address on the map on the initial page load. Search for any IP addresses or domains and see the key information and location. View the optimal layout for each page depending on their device screen size.</p>
+          <div id="grid-column-3" className="flex-cover">
+            <div className="seyi-div div-2">
+              <div className="content">
+                <h3 className="project-title">Space Traveler's Hub</h3>
+                <p>Space Traveler's Hub is a desktop web application built with React, Bootstrap, Jest, and, CSS that allow users to book rockets and join selected space missions.</p>
+              </div>
+              <div className="img-2">
+                {/* <img src={SpaceImage} alt="A girl smiling" className="width" /> */}
+              </div>
+              {/* <div>
+                <h3 className="project-title">Space Traveler's Hub</h3>
+                <p>Space Traveler's Hub is a desktop web application built with React, Bootstrap, Jest, and, CSS that allow users to book rockets and join selected space missions.</p>
+              </div> */}
             </div>
           </div>
-          <div id="grid-row-4">
-            <div>
-              <img src={UrlImage} alt="" className="width margin-start" />
+          <div id="grid-row-3" className="flex-cover">
+            <div className="seyi-div div-3">
+              <div className="content">
+                <h3 className="project-title">IP Address Tracker</h3>
+                <p>IP Address Tracker is a web applcation built with JavaScript, Api, HTML, and, CSS. Users can see their IP Address on the map on the initial page load. Search for any IP addresses or domains and see the key information and location. View the optimal layout for each page depending on their device screen size.</p>
+              </div>
+              <div className="img-3">
+                {/* <img src={IpImage} alt="A girl smiling" className="width" /> */}
+              </div>
             </div>
-            <div>
-              <h3>URL Shortening API</h3>
-              <p>URL Shortening API is a web app built with JavaScript, Api, HTML, and, CSS. Users can shorten any valid URL, see a list of their shortened links, even after refreshing the browser, copy the shortened link to their clipboard in a single click</p>
+          </div>
+          <div id="grid-row-4" className="flex-cover">
+            <div className="seyi-div div-4">
+              <div className="content">
+                <h3 className="project-title">URL Shortening API</h3>
+                <p>URL Shortening API is a web app built with JavaScript, Api, HTML, and, CSS. Users can shorten any valid URL, see a list of their shortened links, even after refreshing the browser, copy the shortened link to their clipboard in a single click</p>
+              </div>
+              <div className="img-4">
+                {/* <img src={UrlImage} alt="" className="width" /> */}
+              </div>
             </div>
           </div>
         </div>
