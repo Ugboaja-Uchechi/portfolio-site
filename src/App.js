aeb08532cbf7fe7586acf84fbe560eb05ecbde36
Ugboaja Uchechi <74814780+Ugboaja-Uchechi@users.noreply.github.com>
2022-06-17 22:29:03 +0100

    Adjust code

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { createContext, useState } from "react";
+import React from "react";
 import { Routes, Route } from 'react-router-dom';
 import Home from './component/Home';
 import About from "./component/About";
